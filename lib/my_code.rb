84d95810d66df8d1b85565d9162e76da8c4e94ca
Joanna E K <kalemajoanna@gmail.com>
2019-11-08 15:42:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -77,7 +77,7 @@ end
 def reduce_to_all_true(array)
   counter = 0 
   while counter < array.size do 
-   return truthy if array = true
+   return true if array = true
     counter +=1 
   end 
   return false
