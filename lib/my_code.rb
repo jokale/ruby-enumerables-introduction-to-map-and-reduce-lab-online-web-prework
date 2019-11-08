e850e39e44310fe33886f7f75b323190a9afe1b1
Joanna E K <kalemajoanna@gmail.com>
2019-11-08 15:01:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,3 +53,13 @@ def map_to_double(array)
 end 
 final_array
 end 
+
+def map_to_square(array)
+  final_array = []
+  counter = 0 
+  while counter < array.size do 
+    final_array.push()
+    counter += 1
+end 
+final_array
+end 
