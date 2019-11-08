fb215ab021039f78d17620c1b0b4ed74eaea09aa
Joanna E K <kalemajoanna@gmail.com>
2019-11-08 15:19:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -68,7 +68,7 @@ def reduce_to_total(array, starting_point=0)
   total = starting_point
   counter = 0 
   while counter < array.size do 
-   total +=
+   total += (array [counter])
     counter +=1 
   end 
   final_array
