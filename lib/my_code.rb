6a88a163b61e7669d621a0a0f806d5db66dcd46d
Joanna E K <kalemajoanna@gmail.com>
2019-11-08 14:57:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,6 +38,16 @@ def map_to_no_change(array)
   final_array = []
   counter = 0 
   while counter < array.size do 
+    final_array.push(array[counter])
+    counter += 1
+end 
+final_array
+end 
+
+def map_to_no_change(array)
+  final_array = []
+  counter = 0 
+  while counter < array.size do 
     final_array.push()
     counter += 1
 end 
