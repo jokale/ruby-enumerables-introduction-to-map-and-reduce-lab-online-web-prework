16d85f7c7bbf81558eb2595ec300e4f473367a75
Joanna E K <kalemajoanna@gmail.com>
2019-11-08 14:59:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,13 +38,13 @@ def map_to_no_change(array)
   final_array = []
   counter = 0 
   while counter < array.size do 
-    final_array.push(array[counter])
+    final_array.push(array[counter] string)
     counter += 1
 end 
 final_array
 end 
 
-def map_to_no_change(array)
+def map_to_double(array)
   final_array = []
   counter = 0 
   while counter < array.size do 
