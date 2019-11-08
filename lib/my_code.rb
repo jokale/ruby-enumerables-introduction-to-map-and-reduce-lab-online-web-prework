f1365cad029812cd80857c4d4eaf6333d4b04ecb
Joanna E K <kalemajoanna@gmail.com>
2019-11-08 14:46:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,7 +27,7 @@
 def map_to_negativize(array)
   final_array = []
   counter = 0 
-  while counter < array.length do 
+  while counter < array.size do 
     array.push(array[counter] *-1)
     counter += 1
 end 
